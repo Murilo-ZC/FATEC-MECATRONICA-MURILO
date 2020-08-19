26b828d15f588debbeb7adba650f4c5fbaf71074
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-08-19 11:07:28 -0300

    Create pratica5.c

---
+++ Microcontroladores-2020-2/Pratica01/pratica5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int numero, resto;
+  printf("Informe um numero:");
+  scanf("%d", &numero);
+
+  resto = numero % 2;
+
+  if(resto == 0){
+    printf("Número Par!\n");
+  } else {
+    printf("Número Ímpar!\n");
+  }
+
+  printf("Fim do Programa!\n");
+
+  return 0;
+}
