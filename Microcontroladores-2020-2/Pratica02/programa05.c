25ddfdc1495f099a608e85eb42dd17aa4f5e432f
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-08-26 10:35:33 -0300

    Create programa05.c

---
+++ Microcontroladores-2020-2/Pratica02/programa05.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void realizarSoma(){
+  float valor1, valor2, resultado;
+  printf("SOMA\n");
+  printf("Informe 2 operandos:");
+  scanf("%f%f", &valor1, &valor2);
+  resultado = valor1 + valor2;
+  printf("Resultado: %.3f\n", resultado);
+}
+
+void realizarSubtracao(){
+  float valor1, valor2, resultado;
+  printf("SUBTRACAO\n");
+  printf("Informe 2 operandos:");
+  scanf("%f%f", &valor1, &valor2);
+  resultado = valor1 - valor2;
+  printf("Resultado: %.3f\n", resultado);
+}
+
+int main(void) {
+  int operacao;
+  printf("Informe a operacao desejada:");
+  scanf("%i", &operacao);
+
+  switch(operacao){
+    case 1: realizarSoma(); break;
+    case 2: realizarSubtracao(); break;
+    case 3:
+      printf("Multiplicacao\n");
+      break;
+    case 5:
+      printf("SEN\n");
+      break;
+    case 4:
+      printf("DIVISAO\n");
+      break;
+    default:
+      printf("Operacao Invalida!\n");
+      break;
+  }
+
+  return 0;
+}
