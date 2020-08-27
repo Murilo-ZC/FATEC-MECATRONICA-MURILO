bdc45d2004ba4bb0060c3ebc009fa9db0bceb836
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-08-27 10:58:45 -0300

    Create programa06.py

---
+++ LTP2-2020-2/Pratica02/programa06.py
@@ -0,0 +1,17 @@
+#Determinar se um número inteiro é par ou impar
+numero = int(input("Numero:"))
+#Calcula o resto da divisao de numero por 2
+resto = numero % 2
+
+#Condição
+if resto == 0:
+  print("PAR")
+  print("Vencedor")
+  print("Nao desista de seus objetivos")
+  print("O mindset é chave do sucesso!")
+else:
+  print("Impar")
+  print("never give up")
+  print("never desista dos seus dreams")
+
+print("Esse sempre aparece!")
