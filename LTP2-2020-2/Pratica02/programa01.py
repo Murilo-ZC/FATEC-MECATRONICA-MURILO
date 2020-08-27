948a778396a17077377e0480c0df72862100c055
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-08-27 10:16:52 -0300

    Create programa01.py

---
+++ LTP2-2020-2/Pratica02/programa01.py
@@ -0,0 +1,9 @@
+#Função para escrever na tela: print()
+print("Ola Mundo!")
+print('Bom dia!')
+#Função de leitura do teclado - input()
+nome = input("Digite seu nome:")
+'''
+  Isso é um bloco de comentários.
+'''
+print("Bom dia", nome, "!")
