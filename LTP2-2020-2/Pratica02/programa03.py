8f600736312158ff285dd171931408059686e501
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-08-27 10:33:51 -0300

    Create programa03.py

---
+++ LTP2-2020-2/Pratica02/programa03.py
@@ -0,0 +1,5 @@
+#Usuário fala dois números e nós calculamos a média deles
+numero1 = int(input("Numero 1:"))
+numero2 = int(input("Numero 2:"))
+media = (numero1 + numero2) / 2
+print("o resultado da media:", media)
