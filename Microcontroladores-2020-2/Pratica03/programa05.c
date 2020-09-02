3a15d93c750779a407d728a89b1cc569e0abbed1
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-02 10:09:41 -0300

    Create programa05.c

---
+++ Microcontroladores-2020-2/Pratica03/programa05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+  int minhaVariavel;
+  minhaVariavel = 10;
+  printf("Valor da minhaVariavel: %i\n", minhaVariavel); //Apresenta o conteúdo
+  printf("Valor da minhaVariavel: %i\n", &minhaVariavel); //Apresenta o endereço
+  return 0;
+}
