0aa2663aa7421cecb5f0439d0ff700bdfa1d8c70
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-03 10:08:37 -0300

    Create programa02.py

---
+++ LTP2-2020-2/Pratica03/programa02.py
@@ -0,0 +1,13 @@
+#Quanto utilizadao =, é uma ordem de atribuição
+numero_secreto = 32
+
+palpite = int(input('Informe um palpite:'))
+#Quando utilizamos ==, estamos fazendo uma pergunta
+if palpite == numero_secreto:
+  print('Acertou!')
+elif palpite > numero_secreto:
+  print('Chute um número menor!')
+elif palpite < numero_secreto:
+  print('Chute um número maior!')
+else:
+  print('Caso padrão')
