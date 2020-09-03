e1a9d2f6caf409aeb07680bdc7338543bffa47e4
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-03 10:59:49 -0300

    Create programa07.py

---
+++ LTP2-2020-2/Pratica03/programa07.py
@@ -0,0 +1,52 @@
+#Para limpar a tela
+from os import system
+from math import sin, cos, radians
+
+#Nossa calculadora
+
+#Cria uma função
+def mostrar_menu():
+  system('clear')
+  print('0 - Sair')
+  print('+ - Somar')
+  print('- - Subtrair')
+  print('* - Multiplicar')
+  print('/ - Dividir')
+  print('sen - Seno')
+  print('cos - Cosseno')
+  print('elevado - Potência')
+  print('raiz - Calcular a Raiz')
+
+def somar():
+  numero1 = float(input('Numero 1:'))
+  numero2 = float(input('Numero 2:'))
+  resultado = numero1 + numero2
+  print('Resultado da soma:', resultado)
+
+def subtrair():
+  numero1 = float(input('Numero 1:'))
+  numero2 = float(input('Numero 2:'))
+  resultado = numero1 - numero2
+  print('Resultado da subtração:', resultado)
+
+def seno():
+  angulo = float(input("Angulo:"))
+  print("Seno do angulo:", sin(radians(angulo)))
+
+ligado = True
+
+while ligado == True:
+  mostrar_menu()
+  opcao = input('Opcao:')
+  if opcao == '0':
+    ligado = False
+  elif opcao == '+':
+    somar()
+  elif opcao == '-':
+    subtrair()
+  elif opcao == 'sen':
+    seno()
+  
+  input('pressione enter para continuar...')
+
+print('Até logo!')
