29e2b0a0fe341e3a4ae55b1105d1bab0b918a98f
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-15 08:22:56 -0300

    Create programa02.py

---
+++ LTP1-2020-2/Pratica04/programa02.py
@@ -0,0 +1,14 @@
+#Pede para o usuário digitar uma senha e valida ela com a senha secreta
+#Cria uma variavel para guardar a senha
+senha_secreta = '123456'
+
+#Pede para o usuario digitar sua senha
+senha = input('Informe a senha:')
+
+#Verifica se a senha do usuario esta correta
+if senha == senha_secreta:
+  print('Bem vindo Hackerman')
+
+if senha != senha_secreta:
+  print('Acesso negado!')
+
