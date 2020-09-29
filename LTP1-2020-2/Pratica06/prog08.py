dc8d0f9cfd84856120d941288c23129d7ee569cb
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-29 09:07:49 -0300

    Create prog08.py

---
+++ LTP1-2020-2/Pratica06/prog08.py
@@ -0,0 +1,11 @@
+soma = 0
+contador = 0
+
+while contador < 3:
+  numero = int(input("Numero:"))
+  soma = soma + numero
+  contador = contador + 1
+
+media = soma / 3
+
+print("Valor medio:", media)
