b94e7c40e5e150f96f3e45b498c7a4f2a0b97d15
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-29 08:04:58 -0300

    Create prog02.py

---
+++ LTP1-2020-2/Pratica06/prog02.py
@@ -0,0 +1,7 @@
+#Ler dois números informados pelo usuário e apresentar a soma deles
+#Convertendo o texto digitado pelo usuario para um número inteiro
+numero_1 = int(input("Digite um numero:"))
+#Converter o texto digitado pelo usuario para um numero real
+numero_2 = float(input("Digite outro número:"))
+resultado = numero_1 + numero_2
+print(resultado)
