7577ee84c3014d251886b38e59209b0af18588a5
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-09-29 08:11:29 -0300

    Create prog03.py

---
+++ LTP1-2020-2/Pratica06/prog03.py
@@ -0,0 +1,12 @@
+#Operações matemáticas
+numero_1 = float(input("Numero 1:"))
+numero_2 = float(input("Numero 2:"))
+
+print("Soma:", numero_1+numero_2)
+print("Subtração:", numero_1-numero_2)
+print("Multiplicação:", numero_1*numero_2)
+print("Divisão:", numero_1/numero_2)
+print('Divisão Inteira:', numero_1//numero_2)
+print("Resto da Divisão:", numero_1%numero_2)
+print('Potência:', numero_1**numero_2)
+print("Raiz:", numero_1**(1./numero_2))
