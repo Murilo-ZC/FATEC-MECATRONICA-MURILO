7de4feaa32b346bfa191639f660ffb3917955d71
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-10-15 09:52:48 -0300

    Create prog01.py

---
+++ LTP2-2020-2/Pratica07/prog01.py
@@ -0,0 +1,10 @@
+#Cria um dicionario vazio
+dicionario = {}
+
+#Adiciona um valor ao dicionario
+dicionario["eletrico"] = 'pikachu'
+dicionario["fogo"] = "charizard"
+dicionario["agua"] = "blastoise"
+dicionario[1] = "Lalala"
+
+print(dicionario)
