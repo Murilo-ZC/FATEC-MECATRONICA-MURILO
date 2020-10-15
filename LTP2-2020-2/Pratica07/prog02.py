759dc0c1757d23bce15a40ae3e2d1cefb328e36f
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-10-15 10:13:43 -0300

    Create prog02.py

---
+++ LTP2-2020-2/Pratica07/prog02.py
@@ -0,0 +1,15 @@
+#Programa que controla carteira
+carteira = { "despesa" : 0, "receita": 0, "emprestimo" : 0 }
+
+continuar = True
+while continuar:
+  opcao = input("Qual o tipo?").lower()
+  valor = float(input("Valor:"))
+  if opcao in carteira.keys():
+    carteira[opcao] += valor #carteira[opcao] = carteira[opcao] + valor
+  else:
+    print("Opcao informada invalida!")
+  print(carteira)
+  continuar = input("Continuar?") == 's'
+
+#Apresenta o saldo da carteira:
