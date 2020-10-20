3fd449408061d10e3a03f9afc2e0765f3998e6c7
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-10-20 08:42:50 -0300

    Create prog08.py

---
+++ LTP1-2020-2/Pratica08/prog08.py
@@ -0,0 +1,14 @@
+preco = input("Informe um preço:")
+
+posicao = 0
+posicao_da_virgula = "SEM VIRGULA"
+while posicao < len(preco):
+  if preco[posicao] == ',':
+    posicao_da_virgula = posicao
+  posicao = posicao + 1
+
+if posicao_da_virgula == "SEM VIRGULA":
+  print("O valor informado", preco, "reais")
+else:
+  print("O valor informado foi de", preco[:posicao_da_virgula], "reais")
+  print("com", preco[posicao_da_virgula+1:] ,"centavos")
