b2d67822abc21aacb1f052ac06b3922d885dc5f8
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-10-20 08:10:09 -0300

    Create prog02.py

---
+++ LTP1-2020-2/Pratica08/prog02.py
@@ -0,0 +1,12 @@
+palavra = "Kakaroto Goku"
+#Acessa uma posição da palavra
+letra = palavra[0]
+#CUIDADO: não pode acessar uma posição que não existe
+#letra = palavra[1000]
+#Acessa um conjunto de letras de uma string
+letras = palavra[1:5]
+#Acessa um conjunto de letras de uma string
+letras = palavra[5:]
+#Acessa um conjunto de letras de uma string
+letras = palavra[:8]
+print(letras)
