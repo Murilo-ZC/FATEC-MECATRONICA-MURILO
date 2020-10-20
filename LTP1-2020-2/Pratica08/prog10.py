4e12ceab8459c4e2f539ff3e915e38c91bfd0882
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-10-20 08:50:22 -0300

    Create prog10.py

---
+++ LTP1-2020-2/Pratica08/prog10.py
@@ -0,0 +1,18 @@
+preco = input("Preço:")
+real = ""
+centavos = ""
+achei_virgula = False
+for caracter in preco:
+  if caracter != ',':
+    #COPIAR CARACTER
+    if achei_virgula == False:
+      real = real + caracter
+    else:
+      centavos = centavos + caracter
+  else:
+    achei_virgula = True
+
+if achei_virgula == True:
+  print("Reais:", real, "Centavos:", centavos)
+else:
+  print("Reais:", real)
