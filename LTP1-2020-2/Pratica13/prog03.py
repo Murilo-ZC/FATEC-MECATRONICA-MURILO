f03086f5c35ccf84ec244ee6abf10493846be10f
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-11-24 08:43:32 -0300

    Create prog03.py

---
+++ LTP1-2020-2/Pratica13/prog03.py
@@ -0,0 +1,9 @@
+num1 = float(input("Informe um valor:"))
+num2 = float(input("Informe outro valor:"))
+
+if num1 > num2:
+  print("Maior:", num1)
+elif num2 > num1:
+  print("Maior:", num2)
+else:
+  print("São Iguais")
