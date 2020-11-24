abddf22182a6e013a931812109a59ad747123a87
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2020-11-24 08:40:54 -0300

    Create prog02.py

---
+++ LTP1-2020-2/Pratica13/prog02.py
@@ -0,0 +1,13 @@
+num1 = int(input("Informe um valor:"))
+num2 = int(input("Informe outro valor:"))
+
+print("Soma:", num1+num2)
+print("Produto:", num1*num2)
+print("Subtração:", num1-num2)
+print("Divisão:", num1/num2)
+print("Divisão Inteira:", num1//num2)
+print("Resto da divisão:", num1%num2)
+print("POtencição:", num1**num2)
+
+
+
