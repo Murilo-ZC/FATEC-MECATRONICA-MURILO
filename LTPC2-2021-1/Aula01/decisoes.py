397aa5d20802a07f3c23e94ce1e25883cdc3b846
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-02-09 09:43:24 -0300

    Add files via upload

---
+++ LTPC2-2021-1/Aula01/decisoes.py
@@ -0,0 +1,10 @@
+altura_minima = 100
+
+altura_usuario = float(input("Informe sua altura (em cm):"))
+
+if altura_usuario >= altura_minima:
+    print("Usuário pode entrar!")
+else:
+    print("Usuário não pode entrar!")
+
+print("Bem vindo ao Siri Cascudo!")
---
+++ LTPC2-2021-1/Aula01/variaveis.py
@@ -0,0 +1,21 @@
+#Comentário de uma linha
+#Eles servem opara descrever alguma coisa no nosso programa
+#Variáveis
+temperatura = 31
+temperatura = 27
+print(temperatura)
+
+#Inteiras
+altura = 150
+
+#Números reais
+preco = 56.98
+
+#Variáveis do tipo texto - string
+nome = "Murilo"
+
+#Exibir as variáveis
+print("Valor da altura", altura," cm")
+print(f"Valor da altura {altura} cm")
+print(f"Preço: {preco}")
+print(f"Nome: {nome}")
