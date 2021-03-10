2c7bf748dc131e77cf9e69f7ee9b478233dad0e3
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-10 08:22:08 -0300

    Create exemplo_polling.ino

---
+++ Microcontroladores-2021-1/Aula02/exemplo_polling/exemplo_polling.ino
@@ -0,0 +1,35 @@
+#define LED1 14
+#define LED2 12
+#define LED3 13
+#define LED_LIGADO 0
+#define LED_DESLIGADO 1
+#define BOTAO1 5
+#define BOTAO2 4
+#define BOTAO_LIGADO 0
+#define BOTAO_DESLIGADO 1
+
+void inicializa_hardware(){
+  //Configura as saídas
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  //Garante que as saídas estão desligadas
+  digitalWrite(LED1, LED_DESLIGADO);
+  digitalWrite(LED2, LED_DESLIGADO);
+  digitalWrite(LED3, LED_DESLIGADO);
+  //Configurar as entradas
+  pinMode(BOTAO1, INPUT_PULLUP);
+  pinMode(BOTAO2, INPUT_PULLUP);
+}
+
+void setup() {
+  inicializa_hardware();
+}
+
+void loop() {
+  if(digitalRead(BOTAO1) == BOTAO_LIGADO){
+    digitalWrite(LED1, LED_LIGADO);
+  } else {
+    digitalWrite(LED1, LED_DESLIGADO);
+  }
+}
