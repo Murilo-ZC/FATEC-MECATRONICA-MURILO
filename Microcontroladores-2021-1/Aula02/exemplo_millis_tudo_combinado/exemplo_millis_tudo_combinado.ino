ed72eb107f8b115d6cbc2426c94ba73c8f657171
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-10 10:44:47 -0300

    Create exemplo_millis_tudo_combinado.ino

---
+++ Microcontroladores-2021-1/Aula02/exemplo_millis_tudo_combinado/exemplo_millis_tudo_combinado.ino
@@ -0,0 +1,59 @@
+#define LED1 14
+#define LED2 12
+#define LED3 13
+#define LED_LIGADO 0
+#define LED_DESLIGADO 1
+#define BOTAO1 5
+#define BOTAO2 4
+#define BOTAO_LIGADO 0
+#define BOTAO_DESLIGADO 1
+
+void inicializa_hardware(){
+  //Configura as saídas
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  //Garante que as saídas estão desligadas
+  digitalWrite(LED1, LED_DESLIGADO);
+  digitalWrite(LED2, LED_DESLIGADO);
+  digitalWrite(LED3, LED_DESLIGADO);
+  //Configurar as entradas
+  pinMode(BOTAO1, INPUT_PULLUP);
+  pinMode(BOTAO2, INPUT_PULLUP);
+}
+
+long tempo1, tempo2;
+
+ICACHE_RAM_ATTR void mudar_led_int(){
+  digitalWrite(LED3, !digitalRead(LED3));
+}
+
+void inicializa_interrupcoes(){
+  //Para definir uma interrupção
+  attachInterrupt(digitalPinToInterrupt(BOTAO1), mudar_led_int, FALLING);
+
+}
+
+void setup() {
+  inicializa_hardware();
+  inicializa_interrupcoes();
+  //Inicializar o tempo
+  tempo1 = millis();
+  tempo2 = millis();
+}
+
+void loop() {
+  if(millis() - tempo1 >= 500){
+    //Atualizar minha hora anotada
+    tempo1 = millis();
+    //Ação desejada
+    digitalWrite(LED2, !digitalRead(LED2));
+  }
+  if(millis() - tempo2 >= 200){
+    //Atualizar minha hora anotada
+    tempo2 = millis();
+    //Ação desejada
+    digitalWrite(LED1, !digitalRead(LED1));
+  }
+  
+}
