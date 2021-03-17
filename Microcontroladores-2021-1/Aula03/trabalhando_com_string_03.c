9952bb564bcef9cf468ce5f4b8dd94d086697934
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-17 08:15:58 -0300

    Create trabalhando_com_string_03.c

---
+++ Microcontroladores-2021-1/Aula03/trabalhando_com_string_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  char msg[20] = "Hello World\n";
+  //Caracter que finaliza a string
+  msg[5] = '\0';
+  printf("%s\n", msg);
+  printf("Conteudo da String:\n");
+  for(int i = 0; i < 20; i++)
+    printf("[%i]:%c\n", i, msg[i]);
+  //Não podemos fazer atribuições diretas para vetores
+  // msg = "Oi eu sou o Goku!"; //Não funciona em C
+  sprintf(msg, "Oi eu sou o Goku!\n");
+  printf("%s", msg);
+  int tamanho_da_string = strlen(msg);
+  printf("Tamanho da String: %i\n", tamanho_da_string);
+  return 0;
+}
