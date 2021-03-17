a7a09c3f6e895793334a032d4145c26b618ebec7
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-17 09:43:31 -0300

    Create comunicacao_serial_01.ino

---
+++ Microcontroladores-2021-1/Aula03/comunicacao_serial_01/comunicacao_serial_01.ino
@@ -0,0 +1,37 @@
+#define LED1 14
+#define LED2 12
+#define LED3 13
+#define LED_LIGADO 0
+#define LED_DESLIGADO 1
+#define BOTAO1 5
+#define BOTAO2 4
+#define BOTAO_LIGADO 0
+#define BOTAO_DESLIGADO 1
+
+void inicializa_hardware(){
+  //Configura as saídas
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  //Garante que as saídas estão desligadas
+  digitalWrite(LED1, LED_DESLIGADO);
+  digitalWrite(LED2, LED_DESLIGADO);
+  digitalWrite(LED3, LED_DESLIGADO);
+  //Configurar as entradas
+  pinMode(BOTAO1, INPUT_PULLUP);
+  pinMode(BOTAO2, INPUT_PULLUP);
+
+  //Inicializa a comunicação Serial
+  Serial.begin(9600); //9600, 19200, 38400 (bluetooth), 115200 (ethernet)
+}
+
+
+void setup() {
+  inicializa_hardware();
+  Serial.println("Ola Mundo Serial!");
+}
+
+void loop() {
+ 
+  
+}
