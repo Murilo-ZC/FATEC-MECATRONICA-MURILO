aeb01a76e07832dd5bc86aa9179d9bbae1c44480
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-05-11 00:48:11 -0300

    Criação dos primeiros testes com o CherryPy

---
+++ LTPC2-2021-1/TesteCherryPy/Teste3.py
@@ -0,0 +1,19 @@
+#Mesmo funcionamento do teste2, mas agora excolendo quando caracteres a função retorna
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(StringGenerator())
