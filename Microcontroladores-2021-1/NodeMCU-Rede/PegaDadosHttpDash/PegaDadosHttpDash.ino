ab455422dd0bbb18dbdbc211b8384d8c13feb2e9
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-06-02 08:02:20 -0300

    Inserção dos dados faltantes

---
+++ Microcontroladores-2021-1/NodeMCU-Rede/PegaDadosHttpDash/PegaDadosHttpDash.ino
@@ -0,0 +1,77 @@
+/**
+   BasicHTTPClient.ino Modificado
+    Created on: 26.05.2021
+
+*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+//Constantes utilizadas no projeto
+#define NOME_DA_REDE "minhaRede"
+#define SENHA_DA_REDE "olamundo"
+
+ESP8266WiFiMulti WiFiMulti;
+
+void inicializaComunicacaoWifi() {
+  //Inicializa a comunicação Serial para depuração - receber mensagens
+  //para acompanhar o desenvolvimento.
+  Serial.begin(115200);
+  //Aguarda 4 segundos para estabelecer a conexão de rede
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+  //Conectando Wifi no modo Station
+  WiFi.mode(WIFI_STA);
+  //Configura o nome da rede e a senha da rede
+  WiFiMulti.addAP(NOME_DA_REDE, SENHA_DA_REDE);
+}
+
+
+void setup() {
+  inicializaComunicacaoWifi();
+}
+
+void tratarRequisicaoGetHttp(const char* site_para_realizar_requisicao) {
+    WiFiClient client;
+    HTTPClient http;
+
+    if (http.begin(client, site_para_realizar_requisicao)) {
+
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        //Entra no if se deu bom!
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          //Opcional - para depuração
+          Serial.println(payload);
+        }
+      } else {
+        //A requisicao não deu certo!
+        //Opcional - para depuração
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+      http.end();
+
+    } else {
+      //Opcional - para depuração
+      Serial.printf("[HTTP} Unable to connect\n");
+    }
+
+}
+
+void loop() {
+  //Programa principal - vai ficar sendo executado enquanto o microcontrolador estiver energizado. Wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    //Variáveis para lidar com a requisição HTTP e com o Cliente Wifi
+    const char* site = "http://192.168.1.65:1880/get-status";
+    tratarRequisicaoGetHttp(site);
+  }
+
+  delay(10000);
+}
