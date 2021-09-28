93a009d2a4acc8cc72fcb6857ba84cbd53b48751
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-09-28 09:23:03 -0300

    Criação da calculadora
    Criação da calculadora

--- LTPC2-2021-2/src/server1.py
+++ LTPC2-2021-2/src/server1.py
@@ -12,11 +12,36 @@ def minha_apresentacao():
         <h2>Palmeiras sem mundial!</h2>
         <p>Esse texto vai aqui!</p>
     '''
+@app.route("/nome")
 @app.route("/nome/<nosso_nome>")
-def aparece_meu_nome(nosso_nome):
-    return f'''
-        <h1>Bem vindo {escape(nosso_nome)}</h1>
-    '''
+def aparece_meu_nome(nosso_nome=None):
+    if nosso_nome == None:
+        return f"""
+            <h1>Você não falou seu nome!</h1>
+        """
+    else:
+        return f'''
+            <h1>Bem vindo {escape(nosso_nome)}</h1>
+        '''
+
+@app.route("/calculadora")
+def descreve_calculadora():
+    return render_template('calculadora_word.html')
+
+@app.route("/calculadora/<operacao>/<num1>")
+@app.route("/calculadora/<operacao>/<num1>/<num2>")
+def calculadora(operacao=None, num1=None, num2=None):
+    if operacao == "soma":
+        resultado = float(num1)+float(num2)
+        return f"""
+            <h1>Resultado da {operacao}:{resultado}</h1>
+        """
+    elif operacao == 'menos':
+        resultado = float(num1) - float(num2)
+        return f"""
+                    <h1>Resultado da {operacao}:{resultado}</h1>
+                """
+
 
 @app.route("/pagina")
 def rodar_pagina_html():
